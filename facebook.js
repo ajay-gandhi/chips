abc0dde8a1c40b1b7514c28789cb4490bfb97b24
Ajay Gandhi <aag255@cornell.edu>
2015-09-18 22:55:33 -0400

    Add browser-main communication

--- facebook.js
+++ facebook.js
@@ -86,4 +86,4 @@ module.exports = (function () {
 
   return new FacebookMessenger;
 
-})()
+})();
--- main.js
+++ main.js
@@ -1,12 +1,14 @@
+
 var app           = require('app'),
-    BrowserWindow = require('browser-window');
+    BrowserWindow = require('browser-window'),
+    ipc           = require('ipc');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 var mainWindow = null;
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function() {
   app.quit();
 });
 
@@ -15,4 +17,26 @@ app.on('ready', function() {
   mainWindow = new BrowserWindow({ width: 800, height: 600 });
   mainWindow.openDevTools();
   mainWindow.loadUrl('file://' + __dirname + '/html/index.html');
+});
+
+// Initialize Facebook messager
+var fb_config = require('./test.json'),
+    fb_msg    = require('./facebook');
+
+fb_msg.init(fb_config.username, fb_config.password);
+
+ipc.on('send-message', function(event, service, name, message) {
+
+  if (service === 'facebook') {
+    fb_msg
+      .act('message', {
+        name: name,
+        text: message
+      })
+      .then(function () {
+        event.sender.send('message-sent', true);
+      })
+      .catch(console.error);
+  }
+
 });
